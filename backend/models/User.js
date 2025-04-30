02f569455e873f6820b83cdcdaadcb2979ecf294
jb_frhl <72656353+sirthirrygolooo@users.noreply.github.com>
2025-04-30 20:02:31 +0200

    patch github (email reste pas def)

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   googleId: { type: String, unique: true, sparse: true },
   githubId: { type: String, unique: true, sparse: true },
   displayName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: false },
   googleToken: { type: String },
   githubToken: { type: String },
   refreshToken: { type: String },
