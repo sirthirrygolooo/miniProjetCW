02b9066e68fb8028adbd1a01a9ca752dd914f24c
James Genitrini <jamesgenitrini@gmail.com>
2025-04-30 17:50:59 +0200

    process.env.frontend

--- chat-auth/server.js
+++ chat-auth/server.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:5001'],
+    origin: ['http://localhost:5173', 'http://localhost:5001', process.env.FRONTEND_URL],
     methods: ['GET', 'POST'],
     credentials: true,
   }
